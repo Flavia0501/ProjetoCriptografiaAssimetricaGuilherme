fd390c28fedb3167184a65929fdb794872d5d1ec
Flavia0501 <flavia41670@fiec.edu.br>
2025-10-28 18:53:48 -0300

    teste

---
+++ src/main/java/br/edu/fiec/CriptografiaAssimetrica/CriptografiaAssimetricaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fiec.CriptografiaAssimetrica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptografiaAssimetricaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptografiaAssimetricaApplication.class, args);
+	}
+
+}
